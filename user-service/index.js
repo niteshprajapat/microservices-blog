1909943cd9041493b73a30f66e2224606cc26d30
Nitesh Prajapat <69027822+niteshprajapat@users.noreply.github.com>
2025-09-07 23:08:12 +0530

    Update index.js

--- user-service/index.js
+++ user-service/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.
+
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('User Service DB Connected'))
