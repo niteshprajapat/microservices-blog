edb540cc78b8ffd38d8f6a43f6d3eabbc186622e
Nitesh Prajapat <69027822+niteshprajapat@users.noreply.github.com>
2025-09-15 23:49:34 +0530

    Update index.js

--- user-service/index.js
+++ user-service/index.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/user.routes.js';
+import userRoutes from './routes/user.routes.js'
 
 dotenv.config();
 const app = express();
