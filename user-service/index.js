3122080fb0d6273a88467c3fd6736c19ddf11fe7
Nitesh Prajapat <69027822+niteshprajapat@users.noreply.github.com>
2026-01-05 22:46:22 +0530

    Update index.js

--- user-service/index.js
+++ user-service/index.js
@@ -6,7 +6,7 @@ import userRoutes from './routes/user.routes.js'
 
 dotenv.config();
 const app = express()  ;
-app.use(express.json());
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 
